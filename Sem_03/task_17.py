d36f57c0c969b8d16acffbc442fdbd0a19de8724
Dmitry Zakharov <ya.dimaz@gmail.com>
2024-04-26 17:12:06 +0300

    Exec Sem_02 tasks

--- Sem_03/task_17.py
+++ Sem_03/task_17.py
@@ -1,9 +1,14 @@
-'''
+"""
 Дан список чисел. Определите, сколько в нем
 встречается различных чисел.
 Input: [1, 1, 2, 0, -1, 3, 4, 4]
 Output: 6
-'''
+"""
+from typing import List
 
-list = [1, 1, 2, 0, -1, 3, 4, 4]
-for
+list_in = [1, 1, 2, 0, -1, 3, 4, 4]
+uniq_list = []
+for el in list_in:
+    if el not in uniq_list:
+        uniq_list.append(el)
+print(len(uniq_list))
--- Sem_03/task_21.py
+++ Sem_03/task_21.py
@@ -1,8 +1,19 @@
-'''
+"""
 Напишите программу для печати всех уникальных
 значений в словаре.
 Input: [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"},
 {"VI": "S005"}, {"VII": " S005 "}, {" V ":" S009 "}, {" VIII
 ":" S007 "}]
 Output: {'S005', 'S002', 'S007', 'S001', 'S009'}
-'''
+"""
+
+list_in = [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"}, {"VI": "S005"}, {"VII": " S005 "}, {" V ": " S009 "},
+           {" VIII": " S007 "}]
+uniq = []
+
+for el in list_in:
+    for item in el:
+        # print(el[item])
+        uniq.append(el[item])
+uniq = set(uniq)
+print(uniq)
