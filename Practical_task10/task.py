d741cd3b036fadaba4927651d0fb401e32796d0a
Dmitry Zakharov <ya.dimaz@gmail.com>
2024-06-11 11:51:28 +0300

    Execute Practical task 10

--- Practical_task10/task.py
+++ Practical_task10/task.py
@@ -17,4 +17,18 @@ lst = ['robot'] * 10
 lst += ['human'] * 10
 random.shuffle(lst)
 data = pd.DataFrame({'whoAmI': lst})
-data.head()
+
+print(f"Сгенерированный DataFrame\n{data.head()}")
+
+# one-hot кодирования с get_dummies
+print(f"\none-hot кодирования с get_dummies\n{pd.get_dummies(data, columns=['whoAmI']).head()}")
+
+# one-hot кодирование без get_dummies
+date_one_hot = pd.DataFrame()
+unique_values = data['whoAmI'].unique()
+# Создание столбцов для каждого уникального значения и заполнение их 0 или 1
+for value in unique_values:
+    date_one_hot[value] = [1 if x == value else 0 for x in data['whoAmI']]
+
+# Вывод результата
+print(f"\none-hot кодирование без get_dummies\n{date_one_hot.head()}")
--- Sem_10/task_67.py
+++ Sem_10/task_67.py
@@ -11,8 +11,22 @@ penguins.head()
 """
 
 from pandas import read_csv
+from seaborn import histplot
+from matplotlib.pyplot import show
 
 penguins = read_csv('penguins.csv')
 
 penguins.loc[penguins['bill_length_mm'] < 35, 'height_group'] = 'low'
 
+penguins.loc[(penguins['bill_length_mm'] > 35) & (penguins['bill_length_mm'] < 42), 'height_group'] = 'middle'
+
+penguins.loc[penguins['bill_length_mm'] > 42, 'height_group'] = 'high'
+
+penguins.to_csv('penguins.csv', index=False)
+
+"""
+Изобразить гистограмму по flipper_length_mm с оттенком height_group. Сделать анализ
+"""
+
+histplot(penguins, x='flipper_length_mm', hue='height_group')
+show()
